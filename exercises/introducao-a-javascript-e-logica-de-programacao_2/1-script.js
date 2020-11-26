96cb479f5075d6c47aac9c21f6dc8ce98d53aad0
MZTBTG <mztbtg@hotmail.com>
2020-11-26 18:05:48 -0300

    script 1: Concluído

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/1-script.js
@@ -0,0 +1,5 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let fator = 0; fator < numbers.length; fator += 1) {
+    console.log("O número da posição " + fator + " é o " + numbers[fator] + "!")
+}
