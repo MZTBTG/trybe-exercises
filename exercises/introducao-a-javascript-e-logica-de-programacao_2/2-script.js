afd4b38679228327cf644da692ae14f667980fb9
MZTBTG <mztbtg@hotmail.com>
2020-11-26 18:14:55 -0300

    script 2: Concluído

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/2-script.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for(let fator = 0; fator < numbers.length; fator += 1) {
+   total += numbers[fator];
+};
+
+console.log('A soma total dos  ' + numbers.length + ' números é ' + total + '!')
