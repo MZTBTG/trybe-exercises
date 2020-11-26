38e5db51c5b2f3843f54ae5c63abfc01ca4b3d89
MZTBTG <mztbtg@hotmail.com>
2020-11-26 18:34:30 -0300

    script 5: Concluído

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/5-script.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let verificador = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] > verificador) {
+        verificador = numbers[index];
+    }
+};
+
+console.log('O maior número do Array é ' + verificador + '!')
